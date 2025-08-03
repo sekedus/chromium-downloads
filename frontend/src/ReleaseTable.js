753fac4e09ee70ce4baf5a8e2c7de7c37de51c20
Sekedus <sekedus@gmail.com>
2025-08-03 15:56:00 +0800

    Add: table pagination

--- frontend/src/ReleaseTable.js
+++ frontend/src/ReleaseTable.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Tag, Breadcrumbs, H2, Card, NonIdealState, Button, Spinner, HTMLTable } from '@blueprintjs/core';
+import { Tag, Breadcrumbs, H2, Card, NonIdealState, Button, Spinner, HTMLTable, HTMLSelect, NumericInput, ControlGroup, ButtonGroup } from '@blueprintjs/core';
 import { Link } from 'react-router-dom'
 import { osInfo, channelInfo } from './util'
 import ReleaseFilter from './ReleaseFilter'
@@ -14,7 +14,9 @@ export default class ReleaseTable extends React.Component {
                 'os': [],
                 'channel': [],
                 'majorVersion': []
-            }
+            },
+            currentPage: 1,
+            rowsPerPage: 20
         }
     }
 
@@ -68,6 +70,12 @@ export default class ReleaseTable extends React.Component {
     _renderReleases() {
         const title = document.title = `Latest Releases${this._getOs() ? ` for ${osInfo[this._getOs()].name}` : ''}`
         const filteredReleases = this._getFilteredReleases()
+        const { currentPage, rowsPerPage } = this.state
+        const totalPages = Math.ceil(filteredReleases.length / rowsPerPage)
+        const paginatedReleases = filteredReleases.slice(
+            (currentPage - 1) * rowsPerPage,
+            currentPage * rowsPerPage
+        )
         return (
             <>
                 {this._getOs() && <Breadcrumbs items={
@@ -84,21 +92,23 @@ export default class ReleaseTable extends React.Component {
                                     this.setState({
                                         filters: Object.assign({}, this.state.filters, {
                                             [type]: this.state.filters[type].filter(key2 => value || key2 !== key).concat(value ? [key] : [])
-                                        })
+                                        }),
+                                        currentPage: 1
                                     })
                                 }}
                                 onClearFilters={() => {
                                     this.setState({
                                         filters: {
                                             os: [], channel: [], majorVersion: []
-                                        }
+                                        },
+                                        currentPage: 1
                                     })
                                 }}
                                 />
 
                 <H2 style={{display: 'inline-block'}}>{title}</H2>
 
-                <HTMLTable bordered condensed striped style={{width: '100%'}}>
+                <HTMLTable bordered condensed striped>
                     <thead>
                         <tr>
                             <th>Version</th>
@@ -109,7 +119,7 @@ export default class ReleaseTable extends React.Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {filteredReleases.length !== 0 ? filteredReleases.map(release => {
+                        {paginatedReleases.length !== 0 ? paginatedReleases.map(release => {
                             return (
                                 <tr key={`${release.version} ${release.os} ${release.timestamp}`}>
                                     <td>{release.version}</td>
@@ -130,6 +140,56 @@ export default class ReleaseTable extends React.Component {
                         </tr>}
                     </tbody>
                 </HTMLTable>
+
+                {paginatedReleases.length !== 0 && <ControlGroup className='table-pagination'>
+                    <ButtonGroup>
+                        <Button
+                            icon="chevron-left"
+                            disabled={currentPage === 1}
+                            onClick={() => this.setState({ currentPage: currentPage - 1 })}
+                        >
+                            Previous
+                        </Button>
+
+                        <ControlGroup style={{ alignItems: 'center', margin: '0 1em' }}>
+                            Page
+                            <NumericInput
+                                min={1}
+                                max={totalPages}
+                                value={currentPage}
+                                stepSize={1}
+                                clampValueOnBlur
+                                allowNumericCharactersOnly
+                                onValueChange={(num) => {
+                                    if (!isNaN(num) && num >= 1 && num <= totalPages) {
+                                        this.setState({ currentPage: num });
+                                    }
+                                }}
+                                style={{ maxWidth: 51 }}
+                            />
+                            of {totalPages}
+                        </ControlGroup>
+
+                        <Button
+                            rightIcon="chevron-right"
+                            disabled={currentPage >= totalPages}
+                            onClick={() => this.setState({ currentPage: currentPage + 1 })}
+                        >
+                            Next
+                        </Button>
+                    </ButtonGroup>
+
+                    <ControlGroup style={{ alignItems: 'center' }}>
+                        Rows per page:&nbsp;
+                        <HTMLSelect
+                            value={rowsPerPage}
+                            onChange={(e) =>
+                                this.setState({ rowsPerPage: Number(e.target.value), currentPage: 1 })
+                            }
+                            options={[20, 50, 100, 200, 500].map(size => ({ value: size, label: `${size}` }))}
+                        />
+                    </ControlGroup>
+                </ControlGroup>}
             </>
         )
     }
